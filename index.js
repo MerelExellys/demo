8c03d025ff02b8b54f9274bfe3bb870e8704118b
MerelExellys <merel.verdegaal@exellys.com>
2022-03-30 10:23:42 +0200

    color > backgroundcolor

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function changeColor(e) {
     var randomColor = Math.floor(Math.random()*16777215).toString(16);
-    e.style.color = `#${randomColor}`;
+    e.style.backgroundColor = `#${randomColor}`;
     console.log(e, randomColor);
 }
